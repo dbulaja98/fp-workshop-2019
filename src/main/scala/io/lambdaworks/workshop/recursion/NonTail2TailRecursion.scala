ce90c1a39223f11f0abda432273a6632007ab908
Dimitrije Bulaja <dbulaja98@gmail.com>
2022-01-21 15:32:22 +0100

    Rewrite factorial as tail-recursive function

--- src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
+++ src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
@@ -1,13 +1,20 @@
 package io.lambdaworks.workshop.recursion
 
+import scala.annotation.tailrec
+
 /**
   * Rewrite below non tail-recursive functions to tail-recursive one.
   * Add @tailrec annotation to prove it.
   */
 object NonTail2TailRecursion {
 
-  def factorial(n: Int): Int =
-    if (n <= 0) 1 else n * factorial(n - 1)
+  def factorial(n: Int): Int = {
+    @tailrec
+    def loop(n: Int, acc: Int): Int =
+      if (n <= 1) acc else loop(n - 1, n * acc)
+
+    loop(n, 1)
+  }
 
   def cubesOfEvens(numbers: List[Double]): List[Double] =
     numbers match {
