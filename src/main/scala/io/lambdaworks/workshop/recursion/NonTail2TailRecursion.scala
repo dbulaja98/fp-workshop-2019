c7fec9035c3d3943afa9f204a748a0c3c7c146b9
Dimitrije Bulaja <dbulaja98@gmail.com>
2022-01-25 14:27:30 +0100

    Recursion (#3)
    * Rewrite factorial as tail-recursive function
    
    * Rewrite cubesOfEvens as tail-recursive function
    
    * Remove idea folder
    
    * Update .gitignore
    
    * Refactor cubesOfEvens method

--- src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
+++ src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
@@ -1,19 +1,29 @@
 package io.lambdaworks.workshop.recursion
 
+import scala.annotation.tailrec
+
 /**
   * Rewrite below non tail-recursive functions to tail-recursive one.
   * Add @tailrec annotation to prove it.
   */
 object NonTail2TailRecursion {
 
-  def factorial(n: Int): Int =
-    if (n <= 0) 1 else n * factorial(n - 1)
+  def factorial(n: Int): Int = {
+    @tailrec
+    def loop(n: Int, acc: Int): Int =
+      if (n <= 1) acc else loop(n - 1, n * acc)
 
-  def cubesOfEvens(numbers: List[Double]): List[Double] =
-    numbers match {
-      case x :: xs if x % 2 == 0 => Math.pow(x, 3) +: cubesOfEvens(xs)
-      case _ :: xs => cubesOfEvens(xs)
-      case Nil     => List.empty
+    loop(n, 1)
+  }
+
+  def cubesOfEvens(numbers: List[Double]): List[Double] = {
+    @tailrec
+    def loop(numbers: List[Double], list: List[Double]): List[Double] = numbers match {
+      case Nil => list
+      case x :: xs if x % 2 == 0 => loop(xs, list :+ Math.pow(x, 3))
+      case _ :: xs => loop(xs, list)
     }
 
+    loop(numbers, List())
+  }
 }
