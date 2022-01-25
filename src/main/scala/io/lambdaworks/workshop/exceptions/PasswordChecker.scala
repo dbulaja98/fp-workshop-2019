8e28630b62d4a002f2892523bbc400da8482252a
Dimitrije Bulaja <dbulaja98@gmail.com>
2022-01-25 14:28:42 +0100

    Error handling (#5)
    * Implement validate method
    
    * Refactor validate method

--- src/main/scala/io/lambdaworks/workshop/exceptions/PasswordChecker.scala
+++ src/main/scala/io/lambdaworks/workshop/exceptions/PasswordChecker.scala
@@ -2,15 +2,30 @@ package io.lambdaworks.workshop.exceptions
 
 object PasswordChecker {
 
-  def validate(password: String): Either[List[Throwable], String] = ???
+  val minimumPasswordLength: Int = 5
 
-  private def minNumberOfChars(password: String, length: Int): Either[Throwable, String] = ???
+  def validate(password: String): Either[List[Throwable], String] = {
+    val errors = List(
+      minNumberOfChars(password, minimumPasswordLength), containsUpperCase(password),
+      containsLowerCase(password), containsNumber(password)
+    ).collect{
+      case Left(error) => error
+    }
 
-  private def containsUpperCase(password: String): Either[Throwable, String] = ???
+    if (errors.isEmpty) Right(password) else Left(errors)
+  }
 
-  private def containsLowerCase(password: String): Either[Throwable, String] = ???
+  private def minNumberOfChars(password: String, length: Int): Either[Throwable, String] =
+    if (password.length >= length) Right(password) else Left(InvalidLength)
 
-  private def containsNumber(password: String): Either[Throwable, String] = ???
+  private def containsUpperCase(password: String): Either[Throwable, String] =
+    if (password.exists(ch => ch.isUpper)) Right(password) else Left(MissingUppercase)
+
+  private def containsLowerCase(password: String): Either[Throwable, String] =
+    if (password.exists(ch => ch.isLower)) Right(password) else Left(MissingLowercase)
+
+  private def containsNumber(password: String): Either[Throwable, String] =
+    if (password.exists(ch => ch.isDigit)) Right(password) else Left(MissingNumber)
 
   object InvalidLength    extends Throwable("Password must contain at least 5 characters.")
   object MissingUppercase extends Throwable("Password must contain uppercase letter.")
@@ -18,3 +33,4 @@ object PasswordChecker {
   object MissingNumber    extends Throwable("Password must contain number.")
 
 }
+
